d1e7f85ab988a944f3e13cc8a403ecc5c83a5f17
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-03-13 20:28:50 +0300

    add method to filtrer throu emails

--- app/auth.py
+++ app/auth.py
@@ -1,12 +1,25 @@
-
-from flask import Blueprint,render_template,request,flash
+from .models import User
+from flask import Blueprint,render_template,request,flash,redirect,url_for
+from app import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 auth = Blueprint('auth',__name__)
 
 @auth.route('/login',methods=["GET","POST"])
 def login():
-    data = request.form
-    print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user =User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password,password):
+                flash('Logged in successfully!',category='success')
+            else:
+                flash('Incorrect password,try again',category='error')
+        else:
+                flash('Email does not exist',category='error')
+   
     return render_template("login.html",text="testing")
 
 @auth.route('/logout')
@@ -27,7 +40,11 @@ def sign_up():
         elif len(password) < 7:
             flash('Short password!!!',category='error')
         else:
-            # add user
+            new_user = User(email=email,username=username,password=generate_password_hash(password,method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+             
             flash('Your account has been created successfully',category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("sign_up.html")
