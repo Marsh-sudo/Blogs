3cbd3dd0bf09f2563bffc52f21731a84307e573d
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-03-13 03:17:27 +0300

    created a function

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,24 @@
-
+from importlib.resources import path
 from flask import Flask 
 from  flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 bootstrap = Bootstrap()
+
 
 def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY']
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     #initializing bootstrap
     bootstrap.init_app(app)
+    
 
     from .views import gg
     from .auth import auth
@@ -18,4 +26,14 @@ def create_app():
     app.register_blueprint(gg,url_prefix='/')
     app.register_blueprint(auth,url_prefix='/')
 
+    from .models import User,Blog
+
+    create_database(app)
+
+
     return app
+
+def create_database(app):
+    if not path.exists('app/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
