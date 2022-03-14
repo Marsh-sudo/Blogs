e24c7deb3422c2e505b738f21c43728546033549
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-03-14 20:35:35 +0300

    Add blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -28,12 +28,8 @@ def create_app():
 
     from .models import User,Blog
 
-    create_database(app)
-
+   
 
     return app
 
-def create_database(app):
-    if not path.exists('app/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+  
