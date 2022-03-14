38b3646338114a8c3e4c2e433b555d6e40547be5
Marsh-sudo <kelvinmunyao01@gmail.com>
2022-03-14 20:33:22 +0300

    create a route to home

--- app/auth.py
+++ app/auth.py
@@ -2,6 +2,7 @@ from .models import User
 from flask import Blueprint,render_template,request,flash,redirect,url_for
 from app import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import login_user,logout_user,current_user,login_required
 
 auth = Blueprint('auth',__name__)
 
@@ -15,6 +16,8 @@ def login():
         if user:
             if check_password_hash(user.password,password):
                 flash('Logged in successfully!',category='success')
+                login_user(user,remember=True)
+                return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password,try again',category='error')
         else:
@@ -23,8 +26,10 @@ def login():
     return render_template("login.html",text="testing")
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "logout"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up',methods=["GET","POST"])
 def sign_up():
@@ -43,6 +48,7 @@ def sign_up():
             new_user = User(email=email,username=username,password=generate_password_hash(password,method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user=True,remember=True)
              
             flash('Your account has been created successfully',category='success')
             return redirect(url_for('views.home'))
